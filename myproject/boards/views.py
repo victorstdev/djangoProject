de28bef50f32df063df22ef3343b931d4cd7b523
victorstdev <1101111011001100110>
2020-11-23 14:09:04 -0300

    boards app added

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
 # Create your views here.
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+  board = get_object_or_404(Board, pk=pk) 
+  return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+  board = get_object_or_404(Board, pk=pk)
+
+  if request.method == 'POST':
+    subject = request.POST['subject']
+    message = request.POST['message']
+
+    user = User.objects.first()
+
+    topic = Topic.objects.create(
+      subject = subject,
+      board = board,
+      starter = user
+    )
+
+    post = Post.objects.create(
+      message = message,
+      topic = topic,
+      created_by = user
+    )
+
+    return redirect('board_topics', pk=board.pk)
+
+  return render(request, 'new_topic.html', {'board':board})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import urls
+
+from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('boards/<int:pk>', views.board_topics, name='board_topics'),
+    path('boards/<int:pk>/new', views.new_topic, name='new_topic')
 ]
