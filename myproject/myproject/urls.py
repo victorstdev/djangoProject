ed2db2969a2d7c35f4b8939ed8c28789688bbbc9
victorstdev <victorstdev@gmail.com>
2020-12-13 22:52:59 -0300

    reply topics

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -38,5 +38,7 @@ urlpatterns = [
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
     path('boards/<int:pk>', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new', views.new_topic, name='new_topic'),
+    path('boards/<int:pk>/topics/<int:topic_pk>', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/reply',views.reply_topic,name='reply_topic'),
     path('admin/', admin.site.urls)
 ]
